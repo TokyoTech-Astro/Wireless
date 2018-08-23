a5a18d766f6ecf7495a2c462f8933d127e8c2136
RLook32 <32rlook@gmail.com>
2018-08-23 16:17:17 +0900

    sendall -> send

--- reciever.py
+++ reciever.py
@@ -6,8 +6,8 @@ DESTINATION = ""
 ENCODE = "utf-8"
 
 
-def sendallStr(conn, msg, enc=ENCODE):
-    conn.sendall(msg.encode(encoding=enc))
+def sendStr(conn, msg, enc=ENCODE):
+    conn.send(msg.encode(encoding=enc))
 
 
 def recieveStr(conn, enc=ENCODE, size=1024):
@@ -18,8 +18,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((DESTINATION, 25565))
     while True:
         data = recieveStr(s)
-        print(data)
+        print(data, end="")
         if data == "exit" or "quit":
             s.close()
             sys.exit()
-        sendallStr(s, "s")
+        sendStr(s, "s")
+        print(";")
