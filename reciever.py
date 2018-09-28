0c940c950dceb6ea013aef51f7d758737713c559
RLook32 <32rlook@gmail.com>
2018-09-28 13:04:02 +0900

    フォーマット改定

--- reciever.py
+++ reciever.py
@@ -15,24 +15,39 @@ def recieveStr(conn, enc=ENCODE, size=1024):
     return conn.recv(size).decode(encoding=enc)
 
 
-GPIO.setmode(GPIO.BUS)
+def send(conn, value):
+    conn.send(value.to_bytes(1, 'big'))
+
+
+def recieve(conn, size=256):
+    return int.from_bytes(conn.recv(size), 'big')
+
+
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN, GPIO.OUT)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((DESTINATION, 25565))
-    while True:
-        data = recieveStr(s)
-        print(data)
-        if data == "on":
-            GPIO.output(PIN, GPIO.HIGH)
-            print(" -> DONE", end="")
-        elif data == "off":
-            GPIO.output(PIN, GPIO.LOW)
-            print(" -> DONE", end="")
-        sendStr(s, "s")
-        if data == "exit" or data == "quit":
-            print()
-            s.close()
-            GPIO.cleanup()
-            sys.exit()
-        print(";")
+    s.recieve = recieve
+
+    try:
+        while True:
+            data = recieveStr(s)
+            print(data)
+            if data & 0b10000000 == 0b10000000:
+                pin = data ^ 0b10000000
+                GPIO.output(pin, GPIO.HIGH)
+                print("TURN ON: %d -> DONE" % pin)
+            elif data != 0b01111111:
+                pin = data
+                GPIO.output(pin, GPIO.LOW)
+                print("TURN OFF: %d -> DONE" % pin)
+            else:
+                GPIO.cleanup()
+                break
+            send(s, 0)
+    except:
+        print("EXCEPTION WAS THROWN")
+        send(s, 1)
+
+print("EXITING NOW...")
--- server.py
+++ server.py
@@ -12,29 +12,71 @@ def recieveStr(conn, enc=ENCODE, size=1024):
     return conn.recv(size).decode(encoding=enc)
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(('', 25565))
-    s.listen(1)
-    while True:
-        conn, addr = s.accept()
-        print(addr)
-        with conn:
-            while True:
-                inp = input(">")
-                sendStr(conn, inp)
-                data = recieveStr(conn)
-                if data == "f":
-                    print("Failed.")
-                    sendStr(conn, "exit")
-                    sys.exit()
-                elif data == "s":
-                    print("Succeed.")
-                else:
-                    print("Unexpected Responce: {}".format(data))
-                    sendStr(conn, "exit")
-                    conn.close()
-                    sys.exit()
-                if inp == "quit" or inp == "exit":
-                    conn.close()
+def send(conn, value):
+    conn.send(value.to_bytes(1, 'big'))
+
+
+def recieve(conn, size=256):
+    return int.from_bytes(conn.recv(size), 'big')
+
+
+def sendExit(conn):
+    send(conn, 0b01111111)
+
+
+def sendCode(conn, toggle=True, port=0):
+    value = 0
+    if toggle:
+        value = 0b10000000
+    value += port
+    send(conn, value)
+
+
+def communicate(service):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('', 25565))
+        s.listen(1)
+        while True:
+            conn, addr = s.accept()
+            print(addr)
+            with conn:
+                try:
+                    while service(conn):
+                        pass
+                except Exception as e:
+                    send(conn, 0b01111111)
+                    print(e)
                     break
+    print("EXITING NOW...")
+
+
+def defaultService(conn):
+    inp = input("On, off or exit? :")
+    if inp.lower() in "exit":
+        sendExit()
+        return False
+    t = True
+    if inp.lower() in "on":
+        pass
+    elif inp.lower() in "off":
+        t = False
+    else:
+        print("Invalid value.")
+        return True
+    
+    inp = input("Pin? :")
+    pin = 0
+    try:
+        pin = int(inp)
+    except ValueError as e:
+        print("Invalid value.")
+        return True
+    if pin > 128 or pin < 0:
+        print("Invalid value.")
+        return True
+    sendCode(conn, t, pin)
+
+
+if __name__ == '__main__':
+    communicate(mainService)
         
